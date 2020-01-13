67137b1c7f310f9caf3757d285a274a1be4fe78b
Rizkyyudistira12 <59831982+Rizkyyudistira12@users.noreply.github.com>
2020-01-13 21:20:21 +0700

    master

---
+++ Hello.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+var helloWorld = function(name) {
+  return new Promise((resolve, reject) => {
+    if (name === "" || name === undefined || name === null) {
+      reject("You have to provide the name!");
+    } else {
+      var message = "Hello world, " + name;
+      resolve(message);
+    }
+  });
+};
+ 
+console.log(helloWorld(""));
+
+ 
+</script>
+
---
+++ firstpwaproject/README.md
@@ -0,0 +1,6 @@
+# firstpwaproject
+
+Tugas Cloud Computing 17.5A.33 ARS Univerity Bandung
+Nama : Muhammad Doni Panigara (17170061)
+            Beni Saprio Parded (17170070)
+            Muchammad Rizky Yudistira (17170107)
